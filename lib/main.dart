62b83e59cfe87714a5032375405260db44b39ab3
Goel Biju <16869075+GoelBiju@users.noreply.github.com>
2021-03-06 13:44:53 +0000

    Working on Android

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+/* This is free and unencumbered software released into the public domain. */
+
+import 'package:flutter/material.dart';
+import 'package:model_viewer/model_viewer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Model Viewer")),
+        body: ModelViewer(
+          backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+          src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
+          // src: 'etc/assets/Astronaut.glb', // a bundled asset file
+          alt: "A 3D model of an astronaut",
+          ar: true,
+          autoRotate: true,
+          cameraControls: true,
+        ),
+      ),
+    );
+  }
+}
