2caf18ad365e489531ce8ebb2a356ee246a533d7
Safiyya <safiyya.onanuga@hotmail.co.uk>
2021-03-06 15:56:13 +0000

    Added seller files

---
+++ src/models/seller.js
@@ -0,0 +1,8 @@
+class Seller {
+    constructor(location, name, owner) {
+        this.location = location;
+        this.name = name;
+        this.owner = owner;
+    }
+}
+module.exports = Seller;
---
+++ src/routes/sellerRoutes.js
@@ -0,0 +1,7 @@
+const express  = require('express');
+const sellerController  = require("../controllers/sellerController");
+const router = express.Router();
+
+
+
+module.exports = router;
