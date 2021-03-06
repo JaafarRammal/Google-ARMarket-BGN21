31ef521365ded27603da3e38848095acf496bbc2
Victoria_ <victoriamadjei@gmail.com>
2021-03-06 14:38:51 +0000

    Added db, product, routes files

---
+++ src/models/product.js
@@ -0,0 +1,12 @@
+class Product {
+	constructor(id, colour, size, object_file, price, quantity) {
+		this.id = id;
+		this.colour = colour;
+		this.size = size;
+		this.object_file = object_file;
+		this.price = price;
+		this.quantity = quantity;
+	}
+}
+
+module.exports = Product;
