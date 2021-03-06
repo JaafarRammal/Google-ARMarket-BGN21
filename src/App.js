991045eb2cfdde59972e71b2f7e096714c2de380
Goel Biju <16869075+GoelBiju@users.noreply.github.com>
2021-03-06 20:13:44 +0000

    Uncomment old code

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import "@google/model-viewer";
 import React from "react";
 import "./App.css";
 import ARPage from "./pages/ARPage";
+import Camera from "./utilities/Camera";
 
 function App() {
+  // <div class="imgbox">
+  //   <model-viewer
+  //     class="center-fit"
+  //     src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
+  //     // src="./assets/table-football.glb"
+  //     alt="A 3D model of an astronaut"
+  //     // auto-rotate
+  //     camera-controls
+  //     ar
+  //     ar-scale="auto"
+  //     ar-modes="scene-viewer webxr quick-look"
+  //   >
+  //     <button slot="ar-button">View in Your Space</button>
+  //   </model-viewer>
+  // </div>
   return (
-    // <div class="imgbox">
-    //   <model-viewer
-    //     class="center-fit"
-    //     src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
-    //     // src="./assets/table-football.glb"
-    //     alt="A 3D model of an astronaut"
-    //     // auto-rotate
-    //     camera-controls
-    //     ar
-    //     ar-scale="auto"
-    //     ar-modes="scene-viewer webxr quick-look"
-    //   >
-    //     <button slot="ar-button">View in Your Space</button>
-    //   </model-viewer>
-    // </div>
-    <ARPage />
+    <div>
+      <Camera />
+      <ARPage />
+    </div>
   );
 }
 
