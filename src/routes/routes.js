498da682cf3d0844f3b03dc18c0579aaf19ebb3e
Victoria_ <victoriamadjei@gmail.com>
2021-03-06 14:49:39 +0000

    Updated routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const router = express.Router();
 
-
+router.post('/product', addProduct);
+router.get('/products', getAllProducts);
+router.get('/product/:id', getProduct);
+router.put('/product/:id', updateProduct);
+router.delete('/product/:id', deleteProduct);
 
 module.exports = {
     routes: router
