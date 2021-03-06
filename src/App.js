0e5a6e1845b05090b473941f73919708b349a7f0
Goel Biju <16869075+GoelBiju@users.noreply.github.com>
2021-03-06 22:12:31 +0000

    Revert changes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import "@google/model-viewer";
 import React from "react";
 import "./App.css";
 import ARPage from "./pages/ARPage";
-import Camera from "./utilities/Camera";
+
+// <div class="imgbox">
+//   <model-viewer
+//     class="center-fit"
+//     src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
+//     // src="./assets/table-football.glb"
+//     alt="A 3D model of an astronaut"
+//     // auto-rotate
+//     camera-controls
+//     ar
+//     ar-scale="auto"
+//     ar-modes="scene-viewer webxr quick-look"
+//   >
+//     <button slot="ar-button">View in Your Space</button>
+//   </model-viewer>
+// </div>
 
 function App() {
-  // <div class="imgbox">
-  //   <model-viewer
-  //     class="center-fit"
-  //     src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
-  //     // src="./assets/table-football.glb"
-  //     alt="A 3D model of an astronaut"
-  //     // auto-rotate
-  //     camera-controls
-  //     ar
-  //     ar-scale="auto"
-  //     ar-modes="scene-viewer webxr quick-look"
-  //   >
-  //     <button slot="ar-button">View in Your Space</button>
-  //   </model-viewer>
-  // </div>
   return (
     <div>
-      <Camera />
       <ARPage />
     </div>
   );
--- src/utilities/camera.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Camera = () => {
-  return <div>Place camera here</div>;
-};
-
-export default Camera;
