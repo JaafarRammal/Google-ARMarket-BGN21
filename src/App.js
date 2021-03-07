440d93f2b5656949b679ded298aed75e7b912bea
Goel Biju <16869075+GoelBiju@users.noreply.github.com>
2021-03-07 00:59:19 +0000

    Add UI for adding product

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "@google/model-viewer";
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -10,22 +9,6 @@ import "./App.css";
 import AddProduct from "./pages/AddProduct";
 import Product from "./pages/Product";
 import Products from "./pages/Products";
-
-// <div class="imgbox">
-//   <model-viewer
-//     class="center-fit"
-//     src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
-//     // src="./assets/table-football.glb"
-//     alt="A 3D model of an astronaut"
-//     // auto-rotate
-//     camera-controls
-//     ar
-//     ar-scale="auto"
-//     ar-modes="scene-viewer webxr quick-look"
-//   >
-//     <button slot="ar-button">View in Your Space</button>
-//   </model-viewer>
-// </div>
 
 function App() {
   return (
--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,10 +1,105 @@
+import { Grid, Paper, TextField, Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
 import React from "react";
 
 function AddProduct() {
   return (
-    <div>
-      <h1>Add Product</h1>
-    </div>
+    <Paper square>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h3">Add Product</Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          Add a product to your business:
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            label="Product Name"
+            required
+            id="product-name"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            label="Price"
+            required
+            id="product-price"
+            type="number"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            label="Quantity"
+            required
+            id="product-quantity"
+            type="number"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+        </Grid>
+
+        <Typography>Upload an image of your product:</Typography>
+        <Grid item xs={12}>
+          <input accept="image/*" id="image-file" type="file" hidden />
+          <label htmlFor="image-file">
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<PhotoCamera />}
+              component="span"
+            >
+              Upload Image
+            </Button>
+          </label>
+        </Grid>
+
+        <Typography>Upload the .GLB 3D model of your product:</Typography>
+        <Grid item xs={12}>
+          <input accept=".glb" id="model-file" type="file" hidden />
+          <label htmlFor="model-file">
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<CloudUploadIcon />}
+              component="span"
+            >
+              Upload Model
+            </Button>
+          </label>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button
+            id="add-product"
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon />}
+          >
+            ADD
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
 
